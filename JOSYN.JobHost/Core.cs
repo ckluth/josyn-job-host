0a6251133e6a3c93f3ad02f707a498455fe3512d
Kluth, Christofer <christofer.kluth@hzv.de>
2026-06-16 15:31:17 +0200

    wip: sync [2026-06-16 15:31]

--- JOSYN.JobHost/Core.cs
+++ JOSYN.JobHost/Core.cs
@@ -16,9 +16,9 @@ public sealed class Core : ICore
     /// <inheritdoc/>
     public static async Task<int> Run(string[] args)
     {
+#if DEBUG
         Console.InputEncoding = new UTF8Encoding();
         Console.OutputEncoding = new UTF8Encoding();
-#if DEBUG
         LocalLog.EnableConsoleOutput = true;
 #endif
 
